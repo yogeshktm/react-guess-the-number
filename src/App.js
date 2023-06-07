3d1574d90c5d79b3adc5fc625ee2037c262fb6d8
Yogeshwaran Sundaramoorthy (Photon) <yogeshwaran.sundaramoorthy@photon.in>
2023-06-07 15:58:45 +0530

    name added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="main-header">
+      <p className="top-content">Made with ❤️ by <a target="_blank" href="https://github.com/yogeshktm">Yogeshwaran</a></p>
         GuessIt<p>Guess the random number between 1 to 100</p>
       </header>
       <GuessNumberContainer></GuessNumberContainer>
